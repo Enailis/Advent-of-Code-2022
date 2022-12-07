b8f5c1c6ce9b904b9897eb40462a5836d5f2d148
ENAILIS <enailismail133@gmail.com>
2022-12-07 20:43:54 +0100

    Day7 - add commentaries

--- Day7/part1.py
+++ Day7/part1.py
@@ -2,15 +2,28 @@
 # Read input #
 ##############
 def readInput():
+    toRet = []
     with open('input') as f:
-        return f.read().strip()
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            else:
+                toRet.append(line.split("\n")[0])
+    return toRet
 
-
+# return 2 values
+# folders :
+#   this value contains string of the path to different folders
+#   they are formatted like : dir1/dir2/dir3
+# files :
+#   this value is a dictionary containing path/fileName as key and size as value
+#   they are formatted like : dir1/dir2/fileName
 def getFoldersAndFiles(input):
     files = {}
     folders = set()
     temp = []
-    for line in input.split("\n"):
+    for line in input:
         if line.startswith("$"):
             if line.startswith("$ cd"):
                 if line[5:] == "..":
@@ -29,7 +42,8 @@ def getFoldersAndFiles(input):
         folders.add("/".join(temp))
     return folders, files
 
-
+# go through folders and find files that have the name of the folder in their key
+# return the total size of every file that have a size under 100000
 def getSizes(folders, files):
     totalSize = 0
     for folder in folders:
@@ -44,4 +58,5 @@ def getSizes(folders, files):
 
 if __name__ == '__main__':
     folders, files = getFoldersAndFiles(readInput())
+    print(files)
     print("Part 1 : ", getSizes(folders, files))
--- Day7/part2.py
+++ Day7/part2.py
@@ -2,15 +2,28 @@
 # Read input #
 ##############
 def readInput():
+    toRet = []
     with open('input') as f:
-        return f.read().strip()
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            else:
+                toRet.append(line.split("\n")[0])
+    return toRet
 
-
+# return 2 values
+# folders :
+#   this value contains string of the path to different folders
+#   they are formatted like : dir1/dir2/dir3
+# files :
+#   this value is a dictionary containing path/fileName as key and size as value
+#   they are formatted like : dir1/dir2/fileName
 def getFoldersAndFiles(input):
     files = {}
     folders = set()
     temp = []
-    for line in input.split("\n"):
+    for line in input:
         if line.startswith("$"):
             if line.startswith("$ cd"):
                 if line[5:] == "..":
@@ -29,7 +42,8 @@ def getFoldersAndFiles(input):
         folders.add("/".join(temp))
     return folders, files
 
-
+# go through folders and find files that have the name of the folder in their key
+# return a dictionary of every folder with the name as key and the size as value
 def getSizes(folders, files):
     folderSizes = {}
     for folder in folders:
@@ -44,4 +58,5 @@ def getSizes(folders, files):
 if __name__ == '__main__':
     folders, files = getFoldersAndFiles(readInput())
     fileSizes = getSizes(folders, files)
+    print(fileSizes)
     print("Part 2 : ", min(i for i in fileSizes.values() if 70000000 - fileSizes[""] + i >= 30000000))
